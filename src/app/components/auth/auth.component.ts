246a43cdf858fb07eadda97686b9f63b3b54d5a9
fjgallard <fjdgallardo@gmail.com>
2019-11-09 20:46:24 +0800

    removed close button temporarily

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
@@ -7,13 +7,21 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, AfterViewChecked {
 
   constructor(
     public dialogRef: MatDialogRef<AuthComponent>,
     protected auth: AuthService) { }
 
   ngOnInit() {
+    // document.getElementById('close-btn').style.top = '0';
+  }
+
+  ngAfterViewChecked() {
+    /* const windowHeight = window.outerHeight;
+    const dialogHeight = document.getElementsByClassName('mat-dialog-container')[0].clientHeight;
+    const top = ((windowHeight - dialogHeight) / 2) - 42;
+    document.getElementById('close-btn').style.top = top + 'px'; */
   }
 
   close() {
