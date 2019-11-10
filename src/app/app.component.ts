f0c0362a84d98f864ad5b008b09f161848043858
fjgallard <fjdgallardo@gmail.com>
2019-11-10 20:56:20 +0800

    proper loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth/auth.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { User } from './data/user.model';
 
 @Component({
@@ -8,9 +9,28 @@ import { User } from './data/user.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blog-app';
+  protected loading = true;
+  protected user: User;
+  protected loading$: Observable<boolean>;
 
   constructor(private auth: AuthService) {
+  }
+
+  ngOnInit() {
+    /* this.auth.user$.subscribe(user => {
+      this.user = user;
+      this.loading = false;
+    }); */
+    this.loading$ = this.auth.user$.pipe(
+      switchMap((user: User) => {
+        this.user = user;
+        this.loading = false;
+        return of(true);
+      })
+    );
+
+    this.loading$.subscribe();
   }
 }
