64a99ca5228eca270e214739d250f182bfe14c77
fjgallard <fjdgallardo@gmail.com>
2019-12-22 15:41:54 +0800

    Some more QoL changes

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -7,21 +7,13 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit, AfterViewChecked {
+export class AuthComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<AuthComponent>,
     protected auth: AuthService) { }
 
   ngOnInit() {
-    // document.getElementById('close-btn').style.top = '0';
-  }
-
-  ngAfterViewChecked() {
-    /* const windowHeight = window.outerHeight;
-    const dialogHeight = document.getElementsByClassName('mat-dialog-container')[0].clientHeight;
-    const top = ((windowHeight - dialogHeight) / 2) - 42;
-    document.getElementById('close-btn').style.top = top + 'px'; */
   }
 
   close() {
